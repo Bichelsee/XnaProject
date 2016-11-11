18e3f4ce436a56c421a737f704e7c4670dca8d28
Manuel <Manuel@PC-MANUEL>
2016-11-11 22:16:51 +0100

    sample

---
+++ XnaProject/TreeSimulator/tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSimulator
+{
+    class Tree
+    {
+      
+
+        public Branch branch = new Branch();
+        public Tree()
+        {
+
+        }
+
+        public void LoseApples()
+        {
+            branch.LoseApples();
+        }
+    }
+}
